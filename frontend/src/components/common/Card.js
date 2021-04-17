97e81776108aecd638d5cc660275bd347e4e4e59
agkayster <ejike.chiboka@gmail.com>
2021-04-17 16:46:38 +0100

    added css for hover over images

--- frontend/src/components/common/Card.js
+++ frontend/src/components/common/Card.js
@@ -9,6 +9,7 @@ const Card = ({ title, author, image }) => {
       <div className='card-image'>
         <figure className='image'>
           <img src={image} alt={title} className='card-image' />
+          <p>click to know more about the book</p>
         </figure>
         <div className='card-content'>
           <div className='content'>{author}</div>
