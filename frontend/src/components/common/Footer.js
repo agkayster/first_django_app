1d124146eeadc23184dd12c73be09b7800d1c2cc
agkayster <ejike.chiboka@gmail.com>
2021-04-19 17:49:41 +0100

    more css code

--- frontend/src/components/common/Footer.js
+++ frontend/src/components/common/Footer.js
@@ -8,66 +8,76 @@ class Footer extends Component {
           <div className='row'>
             <div className='span'>
               <div id='footer-social'>
-                <a
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  href='https://www.booksapp.com'>
-                  <i className='fab fa-twitter-square'></i>
-                </a>
-                <a
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  href='https://www.booksapp.com'>
-                  <i className='fab fa-facebook-square'></i>
-                </a>
+                <p className='copyright'>
+                  {' '}
+									© 2021. BooksApp Inc. All rights reserved{' '}
+                </p>
+                <div className="icons">
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://www.booksapp.com'>
+                    <i className='fab fa-twitter-square'></i>
+                  </a>
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://www.booksapp.com'>
+                    <i className='fab fa-facebook-square'></i>
+                  </a>
+                  <a
+                    target='_blank'
+                    rel='noopener noreferrer'
+                    href='https://www.booksapp.com'>
+                    <i className='fab fa-linkedin-in'></i>
+                  </a>
+                </div>
               </div>
-              <p className='copyright'>
-                {' '}
-								© 2021. BooksApp Inc. All rights reserved{' '}
-              </p>
-              <ul id='menu-footer-menu'>
-                <li className='menu-item'>
-                  <a href='https://www.booksapp.com'>
-                    {' '}
-										Privacy Policy
-                  </a>{' '}
-                </li>
-                <li className='menu-item'>
-                  <a href='https://www.booksapp.com'>
-										Cookie Policy
-                  </a>{' '}
-                </li>
-                <li className='menu-item'>
-                  <a href='https://www.booksapp.com'>
-                    {' '}
-										Terms and Conditions{' '}
-                  </a>
-                </li>
-                <li className='menu-item'>
-                  <a href='https://www.booksapp.com'>
-                    {' '}
-										Contact us
-                  </a>{' '}
-                </li>
-                <li className='menu-item'>
-                  <a href='https://www.booksapp.com'>
-                    {' '}
-										Careers{' '}
-                  </a>
-                </li>
-                <li className='menu-item'>
-                  <a href='https://www.booksapp.com'>
-                    {' '}
-										Advertise
-                  </a>{' '}
-                </li>
-                <li className='menu-item'>
-                  <a href='https://www.booksapp.com'>
-                    {' '}
-										Events
-                  </a>{' '}
-                </li>
-              </ul>
+              <div>
+                <ul id='menu-footer-menu'>
+                  <li className='menu-item'>
+                    <a href='https://www.booksapp.com'>
+                      {' '}
+											Privacy Policy
+                    </a>{' '}
+                  </li>
+                  <li className='menu-item'>
+                    <a href='https://www.booksapp.com'>
+											Cookie Policy
+                    </a>{' '}
+                  </li>
+                  <li className='menu-item'>
+                    <a href='https://www.booksapp.com'>
+                      {' '}
+											Terms and Conditions{' '}
+                    </a>
+                  </li>
+                  <li className='menu-item'>
+                    <a href='https://www.booksapp.com'>
+                      {' '}
+											Contact us
+                    </a>{' '}
+                  </li>
+                  <li className='menu-item'>
+                    <a href='https://www.booksapp.com'>
+                      {' '}
+											Careers{' '}
+                    </a>
+                  </li>
+                  <li className='menu-item'>
+                    <a href='https://www.booksapp.com'>
+                      {' '}
+											Advertise
+                    </a>{' '}
+                  </li>
+                  <li className='menu-item'>
+                    <a href='https://www.booksapp.com'>
+                      {' '}
+											Events
+                    </a>{' '}
+                  </li>
+                </ul>
+              </div>
               <div>
                 <p className='copyright'>
                   {' '}
