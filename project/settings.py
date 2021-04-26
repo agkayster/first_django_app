341536f39e0441b2fd22f914eafe3f8d3b54c08e
agkayster <ejike.chiboka@gmail.com>
2021-04-26 14:48:24 +0100

    new book added

--- project/settings.py
+++ project/settings.py
@@ -25,10 +25,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1*qbk6o6ycr!wx@=gh2$g!y0p(+vpi$&0k8#kt(qab5bp#cgqh'
+# SECRET_KEY = '1*qbk6o6ycr!wx@=gh2$g!y0p(+vpi$&0k8#kt(qab5bp#cgqh'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
